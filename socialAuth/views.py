13fdfbc383a6230c3cb6cdfa9be2c64b9da3c113
timlubanga <timlubanga@gmail.com>
2021-03-25 02:54:11 +0300

    bug fixes and clean up

--- socialAuth/views.py
+++ socialAuth/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
-from socialAuth. serializers import GoogleSocialAuthSerializer, TwitterAuthSerializer, FacebookSocialAuthSerializer
+from socialAuth. serializers import GoogleSocialAuthSerializer,  FacebookSocialAuthSerializer
 
 
 class GoogleSocialAuthView(GenericAPIView):
@@ -34,12 +34,3 @@ class FacebookSocialAuthView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         data = ((serializer.validated_data)['auth_token'])
         return Response(data, status=status.HTTP_200_OK)
-
-
-class TwitterSocialAuthView(GenericAPIView):
-    serializer_class = TwitterAuthSerializer
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return Response(serializer.validated_data, status=status.HTTP_200_OK)
