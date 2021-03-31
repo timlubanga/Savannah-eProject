f1a60c9ce1f675adca73a7ad4dc242a14b884577
timlubanga <timlubanga@gmail.com>
2021-03-31 05:04:28 +0300

    configure staticfiles

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -10,13 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
