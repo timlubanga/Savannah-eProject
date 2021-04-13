3b3b9571d1ab356f591e780b752e9cb66f13c749
timlubanga <timlubanga@gmail.com>
2021-04-13 05:33:48 +0300

    configure cors to allow multiple origins

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -73,6 +73,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 # CORS_ALLOWED_ORIGINS = [
 #     "https://example.com",
@@ -101,7 +103,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 if 'HEROKU' in os.environ:
-    
+
     import django_heroku
     django_heroku.settings(locals())
 
